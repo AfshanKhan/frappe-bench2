1085d165ccb5988ffb510c9b312a124e6a7baec1
Afshan <afshan13k@gmail.com>
2020-04-13 00:03:11 +0530

    test-push-v1

--- apps/accounting/accounting/accounts/report/general_ledger/general_ledger.js
+++ apps/accounting/accounting/accounts/report/general_ledger/general_ledger.js
@@ -179,3 +179,9 @@ frappe.ui.form.on('Google Settings', {
 		frm.dashboard.set_headline(__("For more information, {0}.", [`<a href='https://erpnext.com/docs/user/manual/en/erpnext_integration/google_settings'>${__('Click here')}</a>`]));
 	}
 });
+
+frappe.ui.form.on('Google Settings', {
+	refresh: function(frm) {
+		frm.dashboard.set_headline(__("For more information, {0}.", [`<a href='https://erpnext.com/docs/user/manual/en/erpnext_integration/google_settings'>${__('Click here')}</a>`]));
+	}
+});
