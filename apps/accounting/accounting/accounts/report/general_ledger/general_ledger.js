070b15f29f7aeac1f40b7de8092cf2703b46d851
Afshan <afshan13k@gmail.com>
2020-04-13 20:00:04 +0530

    test refactor

--- apps/accounting/accounting/accounts/report/general_ledger/general_ledger.js
+++ apps/accounting/accounting/accounts/report/general_ledger/general_ledger.js
@@ -189,3 +189,4 @@ frappe.ui.form.on('Google Settings', {
 _('Welcome %s, get started with ERPNext in just a few clicks.' % full_name)
 
 _('Welcome %s, get started with ERPNext in just a few clicks.' % full_name)
+_('Welcome %s, get started with ERPNext in just a few clicks.' % full_name)
